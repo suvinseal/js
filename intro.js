c2b591069907725c5de31aeb5f79d4a8a41517ca
suvinseal <suvinseal@gmail.com>
2024-06-11 15:19:55 -0400

    some more practise with if else and console logs

--- intro.js
+++ intro.js
@@ -1,3 +1,24 @@
 // primitive types
 
-34 + 43
+let a = 4;
+let b = 5;
+
+const x = a + b;
+console.log("x =", x);
+
+// ----
+
+let xyz = "yo";
+
+if (xyz === "yo") {
+    console.log("yay");
+}
+
+console.log("-----");
+// try with random
+
+let uu = Math.random();
+if (uu > 0.5) {
+    console.log(uu);
+    console.log("it worked!")
+}
