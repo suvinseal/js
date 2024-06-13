13af1defae556ef0f354f5d308d5971f63bee3de
suvinseal <suvinseal@gmail.com>
2024-06-12 22:23:29 -0400

    practicing arrays

--- intro.js
+++ intro.js
@@ -11,7 +11,7 @@ console.log("x =", x);
 let xyz = "yo";
 
 if (xyz === "yo") {
-    console.log("yay");
+    console.log("yay, it worked #1");
 }
 
 console.log("-----");
@@ -20,5 +20,39 @@ console.log("-----");
 let uu = Math.random();
 if (uu > 0.5) {
     console.log(uu);
-    console.log("it worked!")
+    console.log("it worked! #2");
 }
+
+// ---- logic &&
+
+// const pwd = prompt("enter password: ");
+
+// if (pwd.length > 6 && pwd.indexOf(" ")) {
+//     console.log("password is ok");
+// } else {
+//     console.log("password is not ok");
+// }
+
+// && operator runs before || 
+
+// arrays
+
+let colors = ['red', 'blue', 'green'];
+
+console.log('len of array=', colors.length);
+
+console.log(colors[1]);
+
+// shift or unshift to remove or add elments from beginning of array
+
+let numbers = ['1', '2', '3'];
+
+const combo = colors.concat(numbers);
+
+console.log(combo);
+
+console.log(combo.indexOf('3'));
+
+let newCombo = combo.slice(2,);
+
+console.log(newCombo);
