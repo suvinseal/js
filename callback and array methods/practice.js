2cd74f46ecbec613f2f3b66ca38b57ce72d7fb81
suvinseal <suvinseal@gmail.com>
2024-06-25 01:55:12 -0400

    first commit for call back f

--- callback and array methods/practice.js
+++ callback and array methods/practice.js
@@ -1,5 +1,7 @@
-const nums = [4, 5, 6, 3, 93, 98, 39, 0]
 
+
+const nums = [4, 5, 6, 3, 93, 98, 39, 0]
+// for each element in an array; execute the function for that element 
 nums.forEach(function (el) {
     console.log(el)
 });
