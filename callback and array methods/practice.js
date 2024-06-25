3d08549b3c66196cece2ea5f4367106682c4dfe6
suvinseal <suvinseal@gmail.com>
2024-06-24 20:50:45 -0400

    some more function practice

---
+++ callback and array methods/practice.js
@@ -0,0 +1,6 @@
+const nums = [4, 5, 6, 3, 93, 98, 39, 0]
+
+nums.forEach(function (el) {
+    console.log(el)
+});
+
--- functions/practice.js
+++ functions/practice.js
@@ -1,4 +1,4 @@
-// sample function
+//  function basics 
 
 function twoSum(a, b) {
     return a + b;
@@ -6,3 +6,25 @@ function twoSum(a, b) {
 
 console.log(twoSum(1, 2));
 
+// scope
+
+function collectEggs() {
+    let totalEggs = 6;
+    console.log(totalEggs);
+}
+
+// print in functions
+
+function greet(firstName) {
+    console.log(`hello, ${firstName}`);
+}
+
+greet("Elvin");
+
+// multiple arguements
+
+function greet2(firstName, lastName) {
+    console.log(`hello, ${firstName} ${lastName[0]}`);
+}
+
+greet2("Elvin", "John");
