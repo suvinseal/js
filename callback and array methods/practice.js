17636d3558a682433c887a38da63e10a809dcc0d
suvinseal <suvinseal@gmail.com>
2024-06-28 01:06:46 -0400

    added some and every

--- callback and array methods/practice.js
+++ callback and array methods/practice.js
@@ -36,15 +36,75 @@ console.log("------");
 // set time out
 
 // print statement appears after 3 seconds / good for displaying aletrs or notifications
-setTimeout(() => {
-    console.log("timeout");
-}, 3000);
+// setTimeout(() => {
+//     console.log("timeout");
+// }, 3000);
 
 // clock interval is used to periodically update part of user interface, such as a clock or a status display
 // other use cases include real-time data fetching, monitoring user inactivity, animations ,and polling 
 
-const id = setInterval(() => {
-    console.log(Math.random());
-}, 2000); // 2 seconds 
+// const id = setInterval(() => {
+//     console.log(Math.random());
+// }, 2000); // 2 seconds 
 
 // use clearInterval(id) to stop;
+
+
+// filter
+
+const num3 = [4, 5, 6, 34, 67, 532, 79];
+
+const newNums3 = num3.filter(num => num % 2 === 0);
+
+// const newNums3 = num3.filter(function (el) {
+//     return el % 2 === 0;
+// })
+
+console.log("filtered")
+console.log(num3);
+console.log(newNums3);
+
+console.log("------");
+
+// some and every
+
+const num4 = [4, 5, 6, 34, 67, 532, 79];
+
+console.log(num4.every(num => num > 3));
+
+console.log(num4.some(num => num > 45));
+
+// reduce 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
